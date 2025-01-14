1675bc8053bb029cabb0be9abdd2d2ae65cc42b3
francesca1991-dot <francescalai91@yahoo.com>
2025-01-14 16:38:58 +0100

    aggiungo file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import NavBar from "./components/navbar/BlogNavbar";
 import Footer from "./components/footer/Footer";
-import Home from "./views/home/Home";
-import Blog from "./views/blog/Blog";
-import NewBlogPost from "./views/new/New";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,9 +8,7 @@ function App() {
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/new" element={<NewBlogPost />} />
+      
       </Routes>
       <Footer />
     </Router>
